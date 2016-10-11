d099b47bf67dff5972340839891950c9d5be6ebb
Lars Weimar <Eisbaeeer@gmail.com>
2016-10-11 15:35:48 +0200

    Initial Version 0.0.4
    PingPong Wort Wecker initial version.

---
+++ README.md
@@ -0,0 +1,159 @@
+![Logo](resources/logo.png)
+
+=======================
+
+# ${1:# Wort Wecker PingPong}
+Das hier vorgestellte Projekt basiert auf dem Retro Ping-Pong Spiel von Franzis.
+Dabei wird die Uhrzeit als Text dargestellt.
+Der Basiscode wurde um ein paar Funktionen erweitert, sodass es jetzt möglich ist, 
+2 Weckzeiten zu definieren. Als Weckton wird ein 1,2KHz Ton erzeugt.
+Über 3 Tasten kann die Uhrzeit und die Wechzeiten eingestellt werden.
+Im nomalen Modus kann mit der UP-Taste der Alarm 1 und der DOWN-Taste der Alarm 2 
+An- und Ausgeschaltet werden.
+
+### Zusätzlich benötigte Bauteile
+* Uhrenquarz 32,768 kHz
+* 3 Taster
+* Piezo Signalgeber
+* Stiftleiste z.B. für ISP, Taster, Piezo und Vcc
+
+## Installation
+## Bauteile
+B6 und B7 = Uhrenquarz
+C4 = Taster UP
+C5 = Taster DOWN
+D2 = Taster Stellen
+D0 = (RXD) geplant für NTP
+D1 = (TXD) geplant für NTP
+D3 = Piezo Signalgeber
+
+## Compiler
+Compiliert wurde das ganze im Atmel Studio 7 (7.0.1006)
+Die Fuse muss noch auf den externen Uhrenquarz umgestellt werden. 
+
+LowFuse: 0xE4
+HighFuse: 0xC9
+
+Beispiel avrdude
+```
+# Get fuses with USBasp
+```
+avrdude -c USBasp -p Atmega8 -n -v
+```
+# Ausgabe sieht so aus:
+avrdude: auto set sck period (because given equals null)
+avrdude: AVR device initialized and ready to accept instructions
+
+Reading | ################################################## | 100% 0.02s
+
+avrdude: Device signature = 0x1e9307
+avrdude: safemode: lfuse reads as E4
+avrdude: safemode: hfuse reads as D9
+
+avrdude: safemode: lfuse reads as E4
+avrdude: safemode: hfuse reads as D9
+avrdude: safemode: Fuses OK
+
+avrdude done.  Thank you.
+
+# Nun die Fuses richtig setzen
+```
+avrdude -c USBasp -p Atmega8 -U lfuse:w:0xE4:m -U hfuse:w:0xC9:m
+```
+# Die Ausgabe muss nun so aussehen:
+avrdude: AVR device initialized and ready to accept instructions
+
+Reading | ################################################## | 100% 0.02s
+
+avrdude: Device signature = 0x1e9307
+avrdude: reading input file "0xE4"
+avrdude: writing lfuse (1 bytes):
+
+Writing | ################################################## | 100% 0.02s
+
+avrdude: 1 bytes of lfuse written
+avrdude: verifying lfuse memory against 0xE4:
+avrdude: load data lfuse data from input file 0xE4:
+avrdude: input file 0xE4 contains 1 bytes
+avrdude: reading on-chip lfuse data:
+
+Reading | ################################################## | 100% 0.02s
+
+avrdude: verifying ...
+avrdude: 1 bytes of lfuse verified
+avrdude: reading input file "0xC9"
+avrdude: writing hfuse (1 bytes):
+
+Writing | ################################################## | 100% 0.02s
+
+avrdude: 1 bytes of hfuse written
+avrdude: verifying hfuse memory against 0xC9:
+avrdude: load data hfuse data from input file 0xC9:
+avrdude: input file 0xC9 contains 1 bytes
+avrdude: reading on-chip hfuse data:
+
+Reading | ################################################## | 100% 0.00s
+
+avrdude: verifying ...
+avrdude: 1 bytes of hfuse verified
+
+avrdude: safemode: Fuses OK
+
+avrdude done.  Thank you.
+```
+
+## Benutzung
+Mit langen Druck auf den Taster PD2 kommt man in die Einstellungen. Das Menü sieht wie folgt aus:
+![Alt text](resources/menu.png)
+
+## Druckvorlage
+Die Druckvorlage ist mit dem Tool "Gimp2" erstellt. Diese ist unter resources zu finden.
+
+## Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+Author:     Martin Steppuhn
+* 18.12.2009 Initial version
+
+Editor:		  Lars Weimar
+* 06.10.2016 
+- Update to Atmel Studio 7
+- Delete uart routine
+*07.10.2016 ver.0.0.1
+- Moved Word lines to get Alarm1 and Alarm2 to bottom
+- Added Alarm1 and Alarm2 Vars and Words
+- Added routines to set alarm times
+* 08.10.2016 ver.0.0.2
+- Added ADC to get 2 more inputs on ADC6, ADC7 
+- Added UP/DOWN buttons
+* 09.10.2016 ver.0.0.3
+- Added EEPROM to save alarm times
+* 09.10.2016 ver.0.0.4
+- Changed to 24h clock
+- Bugfix alarm
+
+## Quellen
+Schaltplan: http://www.elo-web.de/xattachment/0911PingPongSchaltbild.jpg
+Ursprungs C-Code: www.emsystech.de
+ 
+## License
+Copyright (c) Martin Steppuhn (www.emsystech.de)
+
+Nur für den privaten Gebrauch / NON-COMMERCIAL USE ONLY
+
+Die Nutzung (auch auszugsweise) ist für den privaten und nichtkommerziellen 
+Gebrauch erlaubt. Eine Veröffentlichung und Weiterverwendung des Quellcodes 
+ist möglich wenn diese Nutzungsbedingungen incl. Copyright beiliegen
+und die Quelle verlinkt ist. (www.emsystech.de)
+
+Bei kommerziellen Absichten nehmen Sie bitte Kontakt mit uns auf 
+(info@emsystech.de)
+
+Keine Gewähr auf Fehlerfreiheit, Vollständigkeit oder Funktion. Benutzung 
+auf eigene Gefahr. Es wird keinerlei Haftung für direkte oder indirekte 
+Personen- oder Sachschäden übernommen.
---
+++ source/main.c
@@ -0,0 +1,621 @@
+//------------------------------------------------------------------------------
+//                                  _            _     
+//                                 | |          | |    
+//      ___ _ __ ___  ___ _   _ ___| |_ ___  ___| |__  
+//     / _ \ '_ ` _ \/ __| | | / __| __/ _ \/ __| '_ \. 
+//    |  __/ | | | | \__ \ |_| \__ \ ||  __/ (__| | | |
+//     \___|_| |_| |_|___/\__, |___/\__\___|\___|_| |_|
+//                         __/ |                       
+//                        |___/    Engineering
+//
+// Filename:    main.c
+// Description: LED-Paint
+//				
+//
+// Copyright (c) Martin Steppuhn (www.emsystech.de)
+//
+// Nur für den privaten Gebrauch / NON-COMMERCIAL USE ONLY
+//
+// Die Nutzung (auch auszugsweise) ist für den privaten und nichtkommerziellen 
+// Gebrauch erlaubt. Eine Veröffentlichung und Weiterverwendung des Quellcodes 
+// ist möglich wenn diese Nutzungsbedingungen incl. Copyright beiliegen
+// und die Quelle verlinkt ist. (www.emsystech.de)
+//
+// Bei kommerziellen Absichten nehmen Sie bitte Kontakt mit uns auf 
+// (info@emsystech.de)
+//
+// Keine Gewähr auf Fehlerfreiheit, Vollständigkeit oder Funktion. Benutzung 
+// auf eigene Gefahr. Es wird keinerlei Haftung für direkte oder indirekte 
+// Personen- oder Sachschäden übernommen.
+//              
+// Author:      Martin Steppuhn
+// History:     18.12.2009 Initial version
+//
+// Editor:		Lars Weimar
+// History:		06.10.2016 
+//				- Update to Atmel Studio 7
+//				- Delete uart routine
+// History:		07.10.2016 ver.0.0.1
+//				- Moved Word lines to get Alarm1 and Alarm2 to bottom
+//				- Added Alarm1 and Alarm2 Vars and Words
+//				- Added routines to set alarm times
+// History:		08.10.2016 ver.0.0.2
+//				- Added ADC to get 2 more inputs on ADC6, ADC7 
+//				- Added UP/DOWN buttons
+// History:		09.10.2016 ver.0.0.3
+//				- Added EEPROM to save alarm times
+// History:		09.10.2016 ver.0.0.4
+//				- Changed to 24h clock
+//				- Bugfix alarm
+//
+//------------------------------------------------------------------------------
+
+/**** Includes ****************************************************************/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "std_c.h"
+#include <avr/eeprom.h>
+
+/**** Preprocessing directives (#define) **************************************/
+
+//=== Portpins ===
+
+#define		DATA_SET		PORTB |=  (1<<4)
+#define		DATA_CLR		PORTB &= ~(1<<4)
+
+#define		CLK_SET		PORTB |=  (1<<3)
+#define		CLK_CLR		PORTB &= ~(1<<3)
+
+#define		STROBE_SET	PORTB |=  (1<<2)
+#define		STROBE_CLR	PORTB &= ~(1<<2)
+
+#define		BUTTON			(!(PIND & (1<<PD2)))
+#define     UP				(!(PIND & (1<<PD0)))
+#define     DOWN			(!(PIND & (1<<PD1)))
+
+//=== Pixels ===
+
+#define		CLEAR_ALL	led[0]=0; led[1]=0; led[2]=0; led[3]=0;	led[4]=0; led[5]=0; led[6]=0; led[7]=0; led[8]=0;	led[9]=0; led[10]=0; led[11]=0
+
+// Zeile 1
+#define		WORD_ES_IST			led[0]|=1; led[1]|=1; led[4]|=1; led[5]|=1; led[6]|=1
+#define		WORD_ZEHN				led[8]|=1; led[9]|=1; led[10]|=1; led[11]|=1
+// Zeile 2
+#define		WORD_FUENF			led[1]|=0x02; led[2]|=0x02; led[3]|=0x02; led[4]|=0x02
+#define		WORD_VOR_1			led[6]|=0x02; led[7]|=0x02; led[8]|=0x02
+// Zeile 3
+#define		WORD_VIERTEL		led[5]|=0x04; led[6]|=0x04; led[7]|=0x04; led[8]|=0x04; led[9]|=0x04; led[10]|=0x04; led[11]|=0x04
+// Zeile 4
+#define		WORD_NACH				led[0]|=0x08; led[1]|=0x08; led[2]|=0x08; led[3]|=0x08
+#define		WORD_VOR_2			led[4]|=0x08; led[5]|=0x08; led[6]|=0x08
+#define		WORD_HALB				led[8]|=0x08; led[9]|=0x08; led[10]|=0x08; led[11]|=0x08
+// Zeile 5
+#define		WORD_5					led[0]|=0x10; led[1]|=0x10; led[2]|=0x10; led[3]|=0x10
+#define		WORD_2					led[6]|=0x10; led[7]|=0x10; led[8]|=0x10; led[9]|=0x10
+#define		WORD_1					led[8]|=0x10; led[9]|=0x10; led[10]|=0x10; led[11]|=0x10
+// Zeile 6
+#define		WORD_7					led[0]|=0x20; led[1]|=0x20; led[2]|=0x20; led[3]|=0x20; led[4]|=0x20; led[5]|=0x20
+#define		WORD_6					led[7]|=0x20; led[8]|=0x20; led[9]|=0x20; led[10]|=0x20; led[11]|=0x20
+// Zeile 7
+#define		WORD_10					led[0]|=0x40; led[1]|=0x40; led[2]|=0x40; led[3]|=0x40
+#define		WORD_9 					led[3]|=0x40; led[4]|=0x40; led[5]|=0x40; led[6]|=0x40
+#define		WORD_4					led[8]|=0x40; led[9]|=0x40; led[10]|=0x40; led[11]|=0x40
+// Zeile 8
+#define		WORD_3					led[0]|=0x80; led[1]|=0x80; led[2]|=0x80; led[3]|=0x80
+#define		WORD_11					led[4]|=0x80; led[5]|=0x80; led[6]|=0x80
+#define		WORD_8					led[8]|=0x80; led[9]|=0x80; led[10]|=0x80; led[11]|=0x80
+// Zeile 9
+#define		WORD_12					led[1]|=0x100; led[2]|=0x100; led[3]|=0x100; led[4]|=0x100; led[5]|=0x100
+#define		WORD_UHR				led[9]|=0x100; led[10]|=0x100; led[11]|=0x100
+#define		WORD_FUNKUHR		led[5]|=0x100; led[6]|=0x100; led[7]|=0x100; led[8]|=0x100; led[9]|=0x100; led[10]|=0x100; led[11]|=0x100
+// Zeile 10
+#define		WORD_EMSYSTECH	led[1]|=0x200; led[2]|=0x200; led[3]|=0x200; led[4]|=0x200; led[5]|=0x200; led[6]|=0x200; led[7]|=0x200; led[8]|=0x200; led[9]|=0x200
+#define     WORD_ALARM1			led[1]|=0x200; led[2]|=0x200; led[3]|=0x200; led[4]|=0x200; led[5]|=0x200; led[6]|=0x200
+#define     WORD_ALARM2			led[1]|=0x200; led[2]|=0x200; led[3]|=0x200; led[4]|=0x200; led[5]|=0x200; led[7]|=0x200
+
+#define		PIC_FRAME				led[0] = 0x3FF; led[1] = 0x201;	led[2]=0x201; led[3]=0x201; led[4]=0x201;	led[5]=0x201; led[6]=0x201; led[7]=0x201; led[8]=0x201; led[9]=0x201; led[10]=0x201; led[11]=0x3FF
+
+/**** Type definitions (typedef) **********************************************/
+
+/**** Global constants ********************************************************/
+
+/**** Global variables ********************************************************/
+
+/**** Local constants  ********************************************************/
+
+const uint8  font[40] = 				// Digits
+{
+	0xFE,0x82,0x82,0xFE,		// 0
+	0x08,0x04,0xFE,0x00,		// 1
+	0xC4,0xA2,0x92,0x8C,		// 2
+	0x82,0x92,0x92,0x7C,		// 3		
+	0x1E,0x10,0xF8,0x10,		// 4
+	0x9E,0x92,0x92,0x62,		// 5
+	0x78,0x94,0x92,0x60,		// 6
+	0x02,0xF2,0x0A,0x06,		// 7
+	0xFE,0x92,0x92,0xFE, 		// 8
+	0x9E,0x92,0x92,0xFE 		// 9
+};
+
+/**** Local variables *********************************************************/
+
+uint8  			c;
+uint16			led[12];
+uint8			col_cnt;
+uint16			col;
+uint8 			button_mem,up_mem,down_mem,up_mem_al,down_mem_al;
+uint8			hour,minute,second;
+uint8           alarm1_hour,alarm1_minute;
+uint8			alarm2_hour,alarm2_minute;
+uint8			alarm;
+uint16			beep;
+uint8			alarm1_enabled,alarm2_enabled;
+uint8			time_setup;
+uint8			time_setup_cnt0;
+uint8			time_setup_cnt1;
+uint8			key_cnt;
+uint16			loop_cnt;
+uint8			sec_flag,key_flag;
+uint8			eeprom_written;
+// define eeprom bytes
+uint8_t			eeAlarm1_hour EEMEM = 0;
+uint8_t			eeAlarm1_minute EEMEM = 0;
+uint8_t			eeAlarm2_hour EEMEM = 0;
+uint8_t			eeAlarm2_minute EEMEM = 0;
+
+/**** Local function prototypes ***********************************************/
+
+
+
+//------------------------------------------------------------------------------
+// Name:		draw_time
+// Function:	Zeit entsprechend darstellen
+//            
+// Parameter: 
+//				time_setup = 1  --> draw minutes
+//				time_setup = 2 --> draw hours
+//				time_setup = 3 --> draw alarm1_minutes
+//				time_setup = 4 --> draw alarm1_hours
+//				time_setup = 5 --> draw alarm2_minutes
+//				time_setup = 6 --> draw alarm2_hours
+// Return:    
+//------------------------------------------------------------------------------
+void draw_time(void)
+{
+	uint8	i,h;
+
+	if(time_setup == 2)
+	{
+		i = (hour / 10) * 4;
+		led[0] = font[i];
+		led[1] = font[i+1];
+		led[2] = font[i+2];
+		led[3] = font[i+3];
+		led[4] = 0;
+		i = (hour % 10) * 4;
+		led[5] = font[i];
+		led[6] = font[i+1];
+		led[7] = font[i+2];
+		led[8] = font[i+3];
+		led[9] = 0;
+
+		if(second & 1)	{	led[10] = 0x44;	led[11] = 0;	}
+			else					{	led[10] = 0;	led[11] = 0x44;	}
+	}
+	else if(time_setup == 1)
+	{
+		if(second & 1)	{	led[0] = 0x44;	led[1] = 0;	}
+			else					{	led[0] = 0;	led[1] = 0x44;	}
+		led[2] = 0;
+		i = (minute / 10) * 4;
+		led[3] = font[i];
+		led[4] = font[i+1];
+		led[5] = font[i+2];
+		led[6] = font[i+3];
+		led[7] = 0;
+		i = (minute % 10) * 4;
+		led[8] = font[i];
+		led[9] = font[i+1];
+		led[10] = font[i+2];
+		led[11] = font[i+3];
+	}
+	else if(time_setup == 4)
+	{
+		i = (alarm1_hour / 10) * 4;
+		led[0] = font[i];
+		led[1] = font[i+1];
+		led[2] = font[i+2];
+		led[3] = font[i+3];
+		led[4] = 0;
+		i = (alarm1_hour % 10) * 4;
+		led[5] = font[i];
+		led[6] = font[i+1];
+		led[7] = font[i+2];
+		led[8] = font[i+3];
+		led[9] = 0;
+
+		if(second & 1)	{	led[10] = 0x44;	led[11] = 0;	}
+		else					{	led[10] = 0;	led[11] = 0x44;	}
+		WORD_ALARM1;
+	}
+	else if(time_setup == 3)
+	{
+		if(second & 1)	{	led[0] = 0x44;	led[1] = 0;	}
+		else					{	led[0] = 0;	led[1] = 0x44;	}
+		led[2] = 0;
+		i = (alarm1_minute / 10) * 4;
+		led[3] = font[i];
+		led[4] = font[i+1];
+		led[5] = font[i+2];
+		led[6] = font[i+3];
+		led[7] = 0;
+		i = (alarm1_minute % 10) * 4;
+		led[8] = font[i];
+		led[9] = font[i+1];
+		led[10] = font[i+2];
+		led[11] = font[i+3];
+		WORD_ALARM1;
+	}
+	else if(time_setup == 6)
+	{
+		i = (alarm2_hour / 10) * 4;
+		led[0] = font[i];
+		led[1] = font[i+1];
+		led[2] = font[i+2];
+		led[3] = font[i+3];
+		led[4] = 0;
+		i = (alarm2_hour % 10) * 4;
+		led[5] = font[i];
+		led[6] = font[i+1];
+		led[7] = font[i+2];
+		led[8] = font[i+3];
+		led[9] = 0;
+
+		if(second & 1)	{	led[10] = 0x44;	led[11] = 0;	}
+		else					{	led[10] = 0;	led[11] = 0x44;	}
+		WORD_ALARM2;
+	}
+	else if(time_setup == 5)
+	{
+		if(second & 1)	{	led[0] = 0x44;	led[1] = 0;	}
+		else					{	led[0] = 0;	led[1] = 0x44;	}
+		led[2] = 0;
+		i = (alarm2_minute / 10) * 4;
+		led[3] = font[i];
+		led[4] = font[i+1];
+		led[5] = font[i+2];
+		led[6] = font[i+3];
+		led[7] = 0;
+		i = (alarm2_minute % 10) * 4;
+		led[8] = font[i];
+		led[9] = font[i+1];
+		led[10] = font[i+2];
+		led[11] = font[i+3];
+		WORD_ALARM2;
+	}
+	else
+	{
+		CLEAR_ALL;
+		WORD_ES_IST;
+		WORD_UHR;
+
+		h = 0;
+		if(     minute <   5)	{	                            					h = hour; }
+		else if(minute <  10)	{	WORD_FUENF;   WORD_NACH; 							h = hour; }
+		else if(minute <  15)	{	WORD_ZEHN;    WORD_NACH; 							h = hour; }
+		else if(minute <  20)	{	WORD_VIERTEL; WORD_NACH; 							h = hour; }
+		else if(minute <  25)	{	WORD_ZEHN;  	WORD_VOR_1;	WORD_HALB;	h = hour + 1; }
+		else if(minute <  30)	{	WORD_FUENF;   WORD_VOR_1; WORD_HALB;	h = hour + 1; }
+		else if(minute <  35)	{	WORD_HALB;  													h = hour + 1; }
+		else if(minute <  40)	{	WORD_FUENF; 	WORD_NACH; 	WORD_HALB;	h = hour + 1; }
+		else if(minute <  45)	{	WORD_ZEHN; 		WORD_NACH; 	WORD_HALB;	h = hour + 1; }
+		else if(minute <  50)	{	WORD_VIERTEL;	WORD_VOR_2;							h = hour + 1; }
+		else if(minute <  55)	{	WORD_ZEHN; 		WORD_VOR_1;							h = hour + 1; }
+		else if(minute <  60)	{	WORD_FUENF; 	WORD_VOR_1;							h = hour + 1; }
+
+		if(     h ==  0) { WORD_12; }
+		else if(h ==  1) { WORD_1;  }
+		else if(h ==  2) { WORD_2;  }
+		else if(h ==  3) { WORD_3;  }
+		else if(h ==  4) { WORD_4;  }
+		else if(h ==  5) { WORD_5;  }
+		else if(h ==  6) { WORD_6;  }
+		else if(h ==  7) { WORD_7;  }
+		else if(h ==  8) { WORD_8;  }
+		else if(h ==  9) { WORD_9;  }
+		else if(h == 10) { WORD_10; }
+		else if(h == 11) { WORD_11; }
+		else if(h == 12) { WORD_12; }
+		else if(h == 13) { WORD_1;  }
+		else if(h == 14) { WORD_2;	}
+		else if(h == 15) { WORD_3;	}
+		else if(h == 16) { WORD_4;	}
+		else if(h == 17) { WORD_5;	}
+		else if(h == 18) { WORD_6;	}
+		else if(h == 19) { WORD_7;	}
+		else if(h == 20) { WORD_8;	}
+		else if(h == 21) { WORD_9;	}
+		else if(h == 22) { WORD_10;	}
+		else if(h == 23) { WORD_11;	}
+			
+		if (alarm1_enabled == true)
+		{
+			WORD_ALARM1;
+		}
+		if (alarm2_enabled == true)
+		{
+			WORD_ALARM2;
+		}
+
+	}
+}
+
+//------------------------------------------------------------------------------
+// Name:      init
+// Function:  Initalize defaults
+//
+// Parameter:
+// Return:
+//------------------------------------------------------------------------------
+void init(void)
+{
+	// Configure Dataport as output
+	DDRC |= ((1<<PC4) | (1<<PC3) | (1<<PC2) | (1<<PC1) | (1<<PC0));
+	DDRD |= ((1<<PD7) | (1<<PD6) | (1<<PD5) | (1<<PD4));
+	DDRB |= ((1<<PB0) | (1<<PB1) | (1<<PB2) | (1<<PB3) | (1<<PB4));
+	
+	// Configure Dataport as input
+	DDRD &= ~((1<<PD0) | (1<<PD1) | (1<<PD2));
+
+	// enable internal pullup
+	PORTD |= ((1<<PD0) | (1<<PD1) | (1<<PD2));
+	SFIOR &= (1<<PUD);
+	
+	// Timer 2 mit externem 32kHz Quarz betreiben
+	ASSR |= (1<<AS2);
+	TCCR2	= (1<<CS22) + (1<<CS20);			// /128 für 1Hz Int
+
+	// Timer 1 für LED INT mit ca. 1,2kHz
+	TCCR1A = 0;
+	TCCR1B = (1<<WGM12) + (1<<CS10);
+	OCR1A = 6667;
+
+	// Timer Interrupts
+
+	TIMSK  = (1<<OCIE1A) + (1<<TOIE2);   // set interrupt mask
+	
+
+	// Set vars inital
+	hour = 5;
+	minute = 59;
+	second = 0;
+	time_setup = 0;
+	
+	// read from EEPROM
+	alarm1_hour = eeprom_read_byte (&eeAlarm1_hour);
+	alarm1_minute = eeprom_read_byte (&eeAlarm1_minute);
+	alarm2_hour = eeprom_read_byte (&eeAlarm2_hour);
+	alarm2_minute = eeprom_read_byte (&eeAlarm2_minute);
+	
+	
+	alarm = false;
+	alarm1_enabled = true;
+	alarm2_enabled = true;
+}
+
+
+//------------------------------------------------------------------------------
+//		MAIN
+//------------------------------------------------------------------------------
+int main(void)
+{
+ 	init();  // Initialize
+	sei();	// Interrupt ein	
+
+	while(1)
+	{
+		if(sec_flag)	//=== 1 Sekunde ===
+		{
+			sec_flag = false;
+			if(BUTTON)
+			{
+				time_setup_cnt1++;
+				if(time_setup_cnt1 > 3)
+				{
+					time_setup++;
+					if(time_setup > 6) time_setup = 0;  // Leave settings after all menus 
+				}
+			}			
+			else
+			{
+				time_setup_cnt0++;
+				if(time_setup_cnt0 > 10) time_setup = 0;  // Set timeout to go back from settings to normal display (10 sec.)
+			
+			if ((!eeprom_written) && (time_setup == 0))
+			{
+			eeprom_written = true;
+			eeprom_write_byte(&eeAlarm1_hour, alarm1_hour);
+			eeprom_write_byte(&eeAlarm1_minute, alarm1_minute);
+			eeprom_write_byte(&eeAlarm2_hour, alarm2_hour);
+			eeprom_write_byte(&eeAlarm2_minute, alarm2_minute);
+			}
+			
+			}
+			draw_time();
+		}
+		
+			
+		if(key_flag)	//=== 10ms ===	
+		{
+			key_flag = false;
+			
+			if (!UP && up_mem && button_mem)
+			{
+				up_mem = false;
+				time_setup_cnt0 = 0;
+				eeprom_written = false;						// eeprom muss noch geschrieben werden!
+				if(time_setup == 1)	minute = (minute<59) ?minute+1 : 0;
+				if(time_setup == 2) hour = (hour < 23) ? hour+1 :	0;
+				if(time_setup == 3)	alarm1_minute = (alarm1_minute<59) ?alarm1_minute+1 : 0;
+				if(time_setup == 4) alarm1_hour = (alarm1_hour < 23) ? alarm1_hour+1 :	0;
+				if(time_setup == 5)	alarm2_minute = (alarm2_minute<59) ?alarm2_minute+1 : 0;
+				if(time_setup == 6) alarm2_hour = (alarm2_hour < 23) ? alarm2_hour+1 :	0;
+			}
+			if (!DOWN && down_mem && button_mem)
+			{
+				down_mem = false;
+				time_setup_cnt0 = 0;
+				eeprom_written = false;						// eeprom muss noch geschrieben werden!
+				if(time_setup == 1)	minute = (minute>0) ?minute-1 : 0;
+				if(time_setup == 2) hour = (hour > 0) ? hour-1 :	0;
+				if(time_setup == 3)	alarm1_minute = (alarm1_minute>0) ?alarm1_minute-1 : 0;
+				if(time_setup == 4) alarm1_hour = (alarm1_hour > 0) ? alarm1_hour-1 :	0;
+				if(time_setup == 5)	alarm2_minute = (alarm2_minute>0) ?alarm2_minute-1 : 0;
+				if(time_setup == 6) alarm2_hour = (alarm2_hour > 0) ? alarm2_hour-1 :	0;
+			}
+			if(BUTTON)
+			{
+				button_mem = true;
+				time_setup_cnt0 = 0;
+			}
+			else
+			{
+				time_setup_cnt1 = 0; 
+			}
+			
+			if (UP)
+			{
+				up_mem_al = true;
+				up_mem = true;
+			} 
+			if (DOWN)
+			{
+				down_mem_al = true;
+				down_mem = true;
+			}
+			
+			if (!UP && up_mem_al)
+			{
+				up_mem_al = false;
+				alarm1_enabled ^= true;
+				PORTC &= ~(1<<PC4);
+			}
+			
+			if (!DOWN && down_mem_al)
+			{
+				down_mem_al = false;
+				alarm2_enabled ^= true;
+				PORTC &= ~(1<<PC4);
+			}
+			
+			draw_time();		
+		}
+		// check alarm time = now
+		if ((alarm1_enabled == true) && (alarm1_hour == hour) && (alarm1_minute == minute))
+		{
+				alarm = true;
+		}	
+		else if ((alarm2_enabled == true) && (alarm2_hour == hour) && (alarm2_minute == minute))
+		{
+				alarm = true;
+		}	
+		 else
+		 {
+			 alarm = false;
+		 }
+		
+	}
+}
+
+
+//------------------------------------------------------------------------------
+// Name:      TIMER2_OVF_vect
+// Function:  TIMER2 Overflow mit 1Hz (32kHz Uhrenquarz /128 /256
+//            
+// Parameter: 
+// Return:    
+//------------------------------------------------------------------------------
+ISR(TIMER2_OVF_vect)    
+{
+	second++;
+	if(second > 59)
+	{
+		second=0;
+		minute++;
+		if(minute > 59)
+		{
+			minute = 0;
+			hour++;
+			if(hour > 23) hour = 0;
+		}
+	}		
+	sec_flag = true;
+}
+
+//------------------------------------------------------------------------------
+// Name:      TIMER1_COMPA_vect
+// Function:  LED-Matrix Refresh mit 1,2kHz -> 100Hz Bildwiderholrate
+//            
+// Parameter: 
+// Return:    
+//------------------------------------------------------------------------------
+ISR(TIMER1_COMPA_vect)    
+{
+	col_cnt++;
+	if(col_cnt > 11)
+	{
+		col_cnt=0;
+		DATA_CLR;
+	}
+	else
+	{
+		DATA_SET;
+	}
+	CLK_SET;
+	CLK_CLR;
+
+	PORTC &= ~0x0F;
+	PORTD &= ~0xF0;
+	PORTB &= ~0x03;
+
+	STROBE_SET;
+	STROBE_CLR;
+
+	col = led[col_cnt];
+
+	PORTC |= col & 0x0F;
+	PORTD |= col & 0xF0;
+	PORTB |= (col >> 8) & 0x03;
+	
+	// Entprellen der Taster
+	key_cnt++;
+	if(key_cnt > 111)
+	{
+		key_flag = true;			// zum sampeln/entprellen der Taste
+		key_cnt=0;
+	}
+	
+	// Alarm tone
+		if (alarm == true)
+		{
+			if (beep < 100)
+			{
+			PORTC ^= (1<<PC4);
+			beep++;	
+			} 
+			else if (beep > 800)
+			{
+			beep = 0;	
+			} else {
+			PORTC &= ~(1<<PC4);	
+			beep++;
+			}
+	
+		}
+		
+}
+
+
+
+
